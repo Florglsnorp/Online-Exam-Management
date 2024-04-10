482a6896665693d13e5449a8cdf1d427f9827dfe
Florglsnorp <nicholaslink234@gmail.com>
2024-04-10 19:59:53 -0400

    75% of pages exist
    The Teacher and Student home pages were made. Progress is being made to include anchor links on the header of each page to relocate. After base pages are made, login/signup are to be made next.

--- main.py
+++ main.py
@@ -11,5 +11,17 @@ conn = engine.connect()
 def start():
     return render_template('index.html')
 
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/studentHome.html')
+def student():
+    return render_template('studentHome.html')
+
+@app.route('/teachHome.html')
+def teacher():
+    return render_template('teachHome.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
